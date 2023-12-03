efe2d87936fc3ccfe63ada17b587ecd5f18b1245
SNG-newy <nikitas.koussis@gmail.com>
2023-12-03 14:11:12 +1100

    [FIX] fix numpy version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 # run the setup
 setup(name='eigenstrapping',
-      version='0.0.1.6',
+      version='0.0.1.7',
       description="For generating surrogate brain maps with spatial autocorrelation using geometric eigenmodes.",
       author='Nikitas C. Koussis, Systems Neuroscience Group Newcastle',
       author_email='nikitas.koussis@gmail.com',
