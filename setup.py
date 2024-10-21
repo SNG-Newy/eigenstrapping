4981954e339e5af5158ece7b8be3f44724571e26
Nikitas Koussis <31430935+nikitas-k@users.noreply.github.com>
2024-10-21 11:40:48 +1100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,11 +5,6 @@
 from setuptools import setup, find_packages
 
 # run the setup
-setup(name='eigenstrapping',
-      version='0.0.1.11',
-      description="For generating surrogate brain maps with spatial autocorrelation using geometric eigenmodes.",
-      author='Nikitas C. Koussis, Systems Neuroscience Group Newcastle',
-      author_email='nikitas.koussis@gmail.com',
-      url='https://github.com/SNG-newy/eigenstrapping',
-      packages=find_packages(),
-      )
+setup(
+    packages=find_packages(),
+)
